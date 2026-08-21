8db7a792d3b9edd6e9d48fd9791eb45354d46dd5
yasith <yasithranjula2003@gmail.com>
2026-08-21 10:53:03 +0530

    Remove wrong files

--- includes/footer.php
+++
@@ -1,9 +0,0 @@
-</main>
-<footer class="site-footer">
-    <div class="container">
-        <p>&copy; <?php echo date('Y'); ?> Bellina Cosmetics. All rights reserved.</p>
-        <p class="footer-admin"><a href="<?php echo isset($assetBase) ? $assetBase : ''; ?>admin/login.php">Admin Login</a></p>
-    </div>
-</footer>
-</body>
-</html>
